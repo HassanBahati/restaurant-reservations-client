fcff82855fd828c20a196175157365972cdd9240
HassanBahati <mukisabahati@gmail.com>
2021-06-15 20:40:07 +0300

    added components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Book from './components/Book';
+import Main from './components/Main';
+import Navbar from './components/Navbar'
+import ThankYou from './components/ThankYou';
 
 function App() {
+  const [page, setPage] = useState(0)
   return (
-<div>Hello World</div>
+<div><Navbar setPage={setPage}/>
+{page===0 ? <Main setPage={setPage} /> : null}
+{page===1 ? <Book setPage={setPage} /> : null}
+{page===2 ? <ThankYou /> : null}
+</div>
   );
 }
 
