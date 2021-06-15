ac09b51d9c4fb55e7c0b748c28ba442f9cffca01
HassanBahati <mukisabahati@gmail.com>
2021-06-15 21:13:26 +0300

    added components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Row, Col, Button } from "reactstrap";
 
-const Main = props => {
+const Main = (props) => {
   return (
     <div>
       <Row noGutters className="text-center align-items-center">
         <Col>
           <p>If yoou're looking for great pizza</p>
+          <i className="fas fa-pizza-slice pizza-slice" style={{fontSize:'3rem' , color:'black'}}></i>
           <Button
             color="none"
             className="book-table-btn"
@@ -18,9 +19,10 @@ const Main = props => {
           </Button>
         </Col>
       </Row>
-      <Row noGutters className='text-center big-img-container'>
-          <Col>
-          <img src={''} alt='cafe' className='big-img' /></Col>
+      <Row noGutters className="text-center big-img-container">
+        <Col>
+          <img src="./images/cafe-img.png" alt="cafe" className="big-img" />
+        </Col>
       </Row>
     </div>
   );
