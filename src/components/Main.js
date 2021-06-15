a1fca41728df71c41bbebd278ec461634251cc29
HassanBahati <mukisabahati@gmail.com>
2021-06-15 20:53:27 +0300

    added components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Button } from "reactstrap";
 
-const Main = _ => {
-    return(
-        <div>
-            <p>Main page</p>
-        </div>
-    )
-}
+const Main = props => {
+  return (
+    <div>
+      <Row noGutters className="text-center align-items-center">
+        <Col>
+          <p>If yoou're looking for great pizza</p>
+          <Button
+            color="none"
+            className="book-table-btn"
+            onClick={(_) => {
+              props.setPage(1);
+            }}
+          >
+            Book a table
+          </Button>
+        </Col>
+      </Row>
+      <Row noGutters className='text-center big-img-container'>
+          <Col>
+          <img src={''} alt='cafe' className='big-img' /></Col>
+      </Row>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
